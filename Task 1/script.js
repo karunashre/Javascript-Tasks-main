281e057215f635edf0906370537960d81f483e0c
karunashre <mkarunashree2022@vitbhopal.ac.in>
2025-04-19 22:34:41 +0530

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,9 +1,12 @@
 function reverseNumber(num) {
-
     const reversed = num.toString().split('').reverse().join('');
-
     return parseInt(reversed) * Math.sign(num);
 }
 
+function handleReverseNumber() {
+    const input = parseInt(document.getElementById('inputNumber').value);
+    const reversedNumber = reverseNumber(input);
+    document.getElementById('result').innerText = reversedNumber;
+}
 const x = 32243;
 console.log(reverseNumber(x)); 
