3820d0b2c5b678b7323a4dbad060ed23cdc97e33
karunashre <mkarunashree2022@vitbhopal.ac.in>
2025-04-19 22:30:08 +0530

    Update script.js

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,6 +1,13 @@
 function alphabetizeString(str) {
-    return str.split('').sort().join('');
+            return str.split('').sort().join('');
 }
+
+function handleAlphabetize() {
+            const input = document.getElementById('inputString').value;
+            const sortedString = alphabetizeString(input);
+            document.getElementById('result').innerText = sortedString;
+}
+
 
 const example = 'webmaster';
 console.log(alphabetizeString(example)); 
